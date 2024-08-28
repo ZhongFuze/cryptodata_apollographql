2864b8b5f109f129a3b21fdf856916c74d518772
ZhongFuze <zella@mask.io>
2024-08-29 01:38:26 +0800

    use `uvicorn.run` replace command `uvicorn app:application --host localhost --port 5000 --workers 4`

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@
 Author: Zella Zhong
 Date: 2024-08-28 19:02:56
 LastEditors: Zella Zhong
-LastEditTime: 2024-08-28 21:08:34
+LastEditTime: 2024-08-29 00:26:00
 FilePath: /cryptodata_apollographql/src/app.py
 Description: 
 '''
@@ -19,31 +19,24 @@ load_dotenv()
 from fastapi import FastAPI
 from strawberry.fastapi import GraphQLRouter
 from strawberry.schema.config import StrawberryConfig
-from logging.config import BaseConfigurator
-
 
 import setting
 import setting.filelogger as logger
-from setting.filelogger import FileHandler, Formatter
+from schema import Query, Mutation
 
-from pprint import pprint
-
-# # from src.graphql.schemas.mutation_schema import Mutation
-# # from src.graphql.schemas.query_schema import Query
-
-# schema = strawberry.Schema(
-#     query=Query,
-#     mutation=Mutation,
-#     config=StrawberryConfig(
-#         auto_camel_case=True
-#     )
-# )
+schema = strawberry.Schema(
+    query=Query,
+    mutation=Mutation,
+    config=StrawberryConfig(
+        auto_camel_case=True
+    )
+)
 
 
 def create_app():
     app = FastAPI()
-    # graphql_app = GraphQLRouter(schema)
-    # app.include_router(graphql_app, prefix="/graphql")
+    graphql_app = GraphQLRouter(schema)
+    app.include_router(graphql_app, prefix="/graphql")
     @app.get("/")
     async def hello_world():
         logging.info("Hello World")
@@ -54,7 +47,6 @@ def create_app():
 def set_log_config():
     uvicorn_log_config = uvicorn.config.LOGGING_CONFIG
     format = "[%(asctime)s - %(levelname)s %(process)s %(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s"
-    # "%(asctime)s - %(levelname)s - %(message)s"
     uvicorn_log_config["formatters"]["access"]["fmt"] = format
     uvicorn_log_config["formatters"]["default"]["fmt"] = format
     return uvicorn_log_config
@@ -62,10 +54,12 @@ def set_log_config():
 application = create_app()
 
 if __name__ == "__main__":
-    config = setting.load_settings(env="development")
+    config = setting.load_settings(env=os.getenv("ENVIRONMENT"))
     if not os.path.exists(config["server"]["log_path"]):
         os.makedirs(config["server"]["log_path"])
 
+    worker_number = config["server"]["process_count"]
     logger.InitLogger(config)
-    print("Starting server...")
-    uvicorn.run("app:application", host="127.0.0.1", port=5000, workers=4, log_level=logging.ERROR)
+    logging.info(f"Starting server {worker_number} process ...")
+    # uvicorn.run("app:application", host="127.0.0.1", port=5000, workers=worker_number, log_level=logging.INFO)
+    uvicorn.run("app:application", host="127.0.0.1", port=5000, reload=True, log_level=logging.INFO)
